048f58dfef90b310e1402e6b3621934c1aecc9cf
jichaofdu <chaojifudan@outlook.com>
2019-07-26 15:39:46 +0800

    Demo for monitoring memory

--- DemoSystem/demo-service-3/src/main/java/demo/service/DemoService.java
+++ DemoSystem/demo-service-3/src/main/java/demo/service/DemoService.java
@@ -1,14 +1,55 @@
 package demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DemoService {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Scheduled(initialDelay=5000, fixedDelay =30000)
+    public void testMemoryPeriodly() {
+        memory();
+    }
+
+    private void memory() {
+        List<int[]> list = new ArrayList<>();
+
+        Runtime run = Runtime.getRuntime();
+        int i = 1;
+        while (true) {
+            int[] arr = new int[1024 * 8];
+            list.add(arr);
+
+            try {
+                Thread.sleep(5);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (i++ % 1000 == 0) {
+
+                System.out.print(
+                        "[Order Service]Max RAM=" + run.maxMemory() / 1024 / 1024 + "M,");
+                System.out.print(
+                        "[Order Service]Allocated RAM=" + run.totalMemory() / 1024 / 1024 + "M,");
+                System.out.print(
+                        "[Order Service]Rest RAM=" + run.freeMemory() / 1024 / 1024 + "M");
+                System.out.println(
+                        "[Order Service]Max available RAM=" + (run.maxMemory() - run.totalMemory() + run.freeMemory()) / 1024 / 1024 + "M");
+            }
+            if(i >= 6000){
+                break;
+            }
+        }
+    }
 
     public String sayHelloToAll(){
         String str5 = restTemplate.getForObject(
